19c9f29e215ed045fd0ec78b621784d09aca2dfd
gautam <gautamkumar1994@gmail.com>
2018-08-06 14:09:26 +0530

    setting for install, import and use react, reactdom , and validator'

--- src/app.js
+++ src/app.js
@@ -1,16 +1,27 @@
-// import './utils.js'
-import  subtract, {square, add} from './utils.js';
+// // import './utils.js'
+// import  subtract, {square, add} from './utils.js';
 
 
-import {isAdult, canDrink} from './person.js'
-import isSenior from './person.js'
+// import {isAdult, canDrink} from './person.js'
+// import isSenior from './person.js'
 
-console.log("app.js is running !!")
-console.log(square(4))
-console.log(add(4,6))
-console.log(subtract(10,8))
+// console.log("app.js is running !!")
+// console.log(square(4))
+// console.log(add(4,6))
+// console.log(subtract(10,8))
 
 
-console.log(isAdult(19))
-console.log(canDrink(17))
-console.log(isSenior(65))
+// console.log(isAdult(19))
+// console.log(canDrink(17))
+// console.log(isSenior(65))
+
+// install import and use
+
+// import validator from 'validator';
+// console.log(validator.isEmail('test@gmail.com'))
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const templat =React.createElement('p',{}, 'testing123')
+ReactDOM.render(templat, document.getElementById('app'))
