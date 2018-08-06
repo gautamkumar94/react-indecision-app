21e824b7f47defa69911b6e451631bedbc291f55
gautam <gautamkumar1994@gmail.com>
2018-08-06 11:58:46 +0530

    webpack configuration, import and export of webpack

---
+++ src/person.js
@@ -0,0 +1,8 @@
+console.log("person.js is running");
+
+export const isAdult = (age) => age>18;
+export const canDrink = (age) => age >18;
+
+const isSenior = (age) => age > 65;
+export default isSenior;
+
