afe7b6a5a49a458a784ce82e761de6c1d5c37082
gautam <gautamkumar1994@gmail.com>
2018-08-07 15:03:40 +0530

    app is broken into webpack export import component

--- src/app.js
+++ src/app.js
@@ -1,27 +1,31 @@
-// // import './utils.js'
-// import  subtract, {square, add} from './utils.js';
-
-
-// import {isAdult, canDrink} from './person.js'
-// import isSenior from './person.js'
-
-// console.log("app.js is running !!")
-// console.log(square(4))
-// console.log(add(4,6))
-// console.log(subtract(10,8))
-
-
-// console.log(isAdult(19))
-// console.log(canDrink(17))
-// console.log(isSenior(65))
-
-// install import and use
-
-// import validator from 'validator';
-// console.log(validator.isEmail('test@gmail.com'))
-
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp'
 
-const templat =<p>This if from jsx</p>
-ReactDOM.render(templat, document.getElementById('app'))
+
+
+
+  
+  
+  
+ 
+  
+
+  
+
+  
+
+  
+
+  
+  // const User = (props) => {
+  //   return (
+  //     <div>
+  //       <p>Name: {props.name}</p>
+  //       <p>Age: {props.age}</p>
+  //     </div>
+  //   );
+  // };
+  
+  ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+  
